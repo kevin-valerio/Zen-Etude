6c62afdc828ce0448d1588ff40bd6af846d01907
Kevin Valerio <kevin.valerio@etu.univ-amu.fr>
2017-12-05 17:21:55 +0100

    Update dump_and_send_to_zenetude.bash

--- dump_and_send_to_zenetude.bash
+++ dump_and_send_to_zenetude.bash
@@ -1,10 +1,19 @@
 #!/bin/bash 
-#Test : Dans la machine Scodoc7Demo.ova
-#Prod : Dans le serveur de l'IUT où est stocké scodoc
 
+ 
+clear; 
+if [ "$(whoami)" != "postgres" ]; then
+	echo "Veuillez vous connecter en tant que postgres !"
+	exit;
+fi
+
+#
+cd /home/ds;
 command -v sshpass >/dev/null 2>&1 || { apt install sshpass; }
 command -v pg_dumpall >/dev/null 2>&1 || {  echo "pg_dumpall n'est pas installé." >&2;  }
-command -v scp >/dev/null 2>&1 || {  echo "scp n'est pas installé." >&2;  }
 
-pg_dumpall > scodoc.dump.txt;
-sshpass -p 'zenetude' scp -p scodoc.dump.txt zenetude@ssh-zenetude.alwaysdata.net:www/db/scodoc.dump.txt
+pg_dumpall > ~/scodoc.dump.txt;
+echo "Dump de la base de donnée.";
+
+sshpass -p 'zenetude' scp /var/lib/postgresql/scodoc.dump.txt zenetude@ssh-zenetude.alwaysdata.net:www/db/scodoc.dump.txt;
+echo "Fin de l'upload";
