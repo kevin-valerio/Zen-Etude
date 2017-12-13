93083a4ed93fb5f4fc15a5dd7c851691771a90c2
Soutif Thomas <th.soutif@gmail.com>
2017-12-13 20:47:46 +0100

    ajout en commentaire d'une option de dump

--- bash/dump_and_send_to_zenetude.bash
+++ bash/dump_and_send_to_zenetude.bash
@@ -13,6 +13,8 @@ command -v sshpass >/dev/null 2>&1 || { apt install sshpass; }
 command -v pg_dumpall >/dev/null 2>&1 || {  echo "pg_dumpall n'est pas installé." >&2;  }
 
 pg_dumpall > ~/scodoc.dump.txt;
+#Dump des données seulement
+#pg_dump --column-inserts --data-only > ~/scodoc.dump.txt;
 echo "Dump de la base de donnée.";
 
 sshpass -p 'zenetude' scp /var/lib/postgresql/scodoc.dump.txt zenetude@ssh-zenetude.alwaysdata.net:www/db/scodoc.dump.txt;
