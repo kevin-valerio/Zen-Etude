4c60c88437a9c317fe4aacfcb34dafc68c9a47d7
Soutif Thomas <th.soutif@gmail.com>
2017-12-13 19:21:14 +0100

    Update import_scodoc-dump_into_db.bash

--- bash/import_scodoc-dump_into_db.bash
+++ bash/import_scodoc-dump_into_db.bash
@@ -1,2 +1,6 @@
 #Dans le serveur Zen'Etude
-
+#!/bin/bash
+clear;
+echo "Exportation du dump de scodoc sur la base de donnée zenetude";
+echo "Entrer le mot de passe de la base de donnée psql : ";
+psql -h postgresql-zenetude.alwaysdata.net -U zenetude -d zenetude_db < scodoc.dump.txt;
