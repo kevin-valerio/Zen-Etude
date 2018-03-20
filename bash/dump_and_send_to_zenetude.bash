35215019cbd0721988a467da882ab25392a8b508
Kevin Valerio <kevin.valerio@etu.univ-amu.fr>
2018-03-20 20:27:38 +0100

    Update dump_and_send_to_zenetude.bash

--- bash/dump_and_send_to_zenetude.bash
+++ bash/dump_and_send_to_zenetude.bash
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
- 
+echo "Lancement de la commande";
+
 clear; 
 if [ "$(whoami)" != "postgres" ]; then
 	echo "Veuillez vous connecter en tant que postgres !"
@@ -14,8 +15,8 @@ command -v pg_dumpall >/dev/null 2>&1 || {  echo "pg_dumpall n'est pas installé
 
 pg_dumpall > ~/scodoc.dump.txt;
 #Dump des données seulement
-#pg_dump --column-inserts --data-only > ~/scodoc.dump.txt;
 echo "Dump de la base de donnée.";
 
+#Premier lancement : executer cette commmande à la main !
 sshpass -p 'zenetude' scp /var/lib/postgresql/scodoc.dump.txt zenetude@ssh-zenetude.alwaysdata.net:www/db/scodoc.dump.txt;
 echo "Fin de l'upload";
