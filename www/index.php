1b49e5f5a4d327f8cd61347007a74780eb0a96e2
Mikael-Salasca <mikael.salasca@etu.univ-amu.fr>
2018-03-21 17:05:26 +0100

    mise en place mvc

---
+++ www/index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+//define("WEBROOT");
+define("ROOT",__DIR__);
+/*
+echo '<pre>';
+print_r($_SERVER);
+echo '</pre>';
+*/
+
+$params = explode('/',$_SERVER['REDIRECT_URL']);
+if (isset($params[1])&& isset($params[2])) {
+
+$controller = $params[1];
+$action = $params[2];
+//echo 'controller : ' . $controller . '</br>';
+//echo 'action : ' . $action;
+if (file_exists('controller/' . $controller . '.php')) {
+require 'controller/' . $controller . '.php';
+if (class_exists($controller)) {
+$controllerObject = new $controller();
+if (method_exists($controllerObject, $action))
+$controllerObject->$action();
+}
+
+}
+else {
+require 'controller/pagenotfound.php';
+$controllerObject = new Pagenotfound();
+$controllerObject->displayError();
+$controllerObject->end_page();
+}
+}
+
+else {
+require 'controller/home.php';
+$controllerObject = new Home();
+$controllerObject->index();
+}
